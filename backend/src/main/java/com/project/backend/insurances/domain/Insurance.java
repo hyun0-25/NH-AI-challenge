1f278068876f1ac9e4d7929d90287979296d91de
hyun0-25 <lisa1072@skku.edu>
2025-09-12 22:44:28 +0900

    [Feat] 농업 재해보험 엔티티 생성 및 데이터 insert

---
+++ backend/src/main/java/com/project/backend/insurances/domain/Insurance.java
@@ -0,0 +1,43 @@
+package com.project.backend.insurances.domain;
+
+import com.project.backend.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "insurance")
+public class Insurance extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long insuranceId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private InsuranceConditionType insuranceConditionType;
+
+    private String insuranceCoverage;
+
+    private String insuranceDisaster;
+
+    private String insuranceVariety;
+
+    private String insurancePurpose;
+
+    private String insuranceStartDate;
+
+    private String insuranceEndDate;
+
+    private String insurancePaymentReason;
+
+    private Integer insuranceGovernmentSupportMin;
+
+    private Integer insuranceGovernmentSupportMax;
+
+    private Integer insuranceLocalGovernmentSupportMin;
+
+    private Integer insuranceLocalGovernmentSupportMax;
+}
